5e390afa09cfe15f3a7075f8e8b2bdc48edb5f63
Dor Shaar <dorsr@mta.ac.il>
2020-04-14 16:55:38 +0300

    UpdateNuget

---
+++ TaskManager/Domain/TaskData.Contracts/ITasksGroup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TaskData.Contracts
+{
+    public interface ITasksGroup
+    {
+        string ID { get; }
+        string Name { get; set; }
+        int Size { get; }
+        bool IsFinished { get; }
+
+        IEnumerable<IWorkTask> GetAllTasks();
+        IWorkTask GetTask(string id);
+        IWorkTask CreateTask(string description);
+        void AddTask(IWorkTask task);
+        void RemoveTask(string id);
+        void RemoveTask(params string[] ids);
+        void UpdateTask(IWorkTask task);
+    }
+}
---
+++ TaskManager/Domain/TaskData.Contracts/ITasksGroupBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Logger.Contracts;
+
+namespace TaskData.Contracts
+{
+     public interface ITasksGroupBuilder
+     {
+          ITasksGroup Create(string groupName, ILogger logger);
+     }
+}
