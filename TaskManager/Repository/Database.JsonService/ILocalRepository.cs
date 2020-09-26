da567993f2290c1e67e51d9b14ccf79e76243dce
Dor <Dor.Shaar@votiro.com>
2020-09-26 14:21:15 +0300

    Add ILocalRepository which was deleted by mistake

---
+++ TaskManager/Repository/Database.JsonService/ILocalRepository.cs
@@ -0,0 +1,19 @@
+﻿using Database.Configuration;
+using System.Collections.Generic;
+
+namespace Databases
+{
+    public interface ILocalRepository<T> : IRepository<T>, IDatabaseLocalConfiguration
+    {
+    }
+
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetEntity(string entity);
+        void Insert(T entity);
+        void Update(T entity);
+        void AddOrUpdate(T entity);
+        void Remove(T entity);
+    }
+}
